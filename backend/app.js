13b5abf0a590529ccff3e57c9736841f5aa1e123
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-02-25 22:17:20 -0300

    Connecting to Mongo DB database

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://thiago:8z27iD2PzEvlIBbJ@cluster0-xumlv.mongodb.net/test?retryWrites=true")
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,7 +31,10 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+   });
   console.log(post);
   res.status(201).json({
     message: "Post added successfully",
