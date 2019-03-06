f72c0752894033bfdb15f4d98c8005abb54852b0
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-03-06 20:28:43 -0300

    Creating routing application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PostListComponent },
+  { path: 'create', component: PostCreateComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
