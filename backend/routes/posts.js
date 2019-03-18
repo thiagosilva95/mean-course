93004b10e37afa253ecb7c67719a4028722f59c1
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-03-17 21:59:59 -0300

    Setting multer

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -13,15 +13,21 @@ const MIME_TYPE_MAP = {
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "backend/images");
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Ivalid mime type");
+    if (isValid) {
+      error = null;
+    }
+    cb(error, "backend/images");
   },
   filename: (req, file, cb) => {
     const name = file.originalname.toLowerCase().split(' ').join('-');
     const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + '-' + Date.now() + '.' + ext);
   }
 });
 
-router.post("", (req, res, next) => {
+router.post("", multer(storage).single("image"), (req, res, next) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content
