fb505f5704a6d41a032943cd4a8e42e0f55796f2
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-04-15 22:02:24 -0300

    Adding login and signup components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,13 +18,17 @@ import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { HeaderComponent } from "./header/header.component";
 import { PostListComponent } from "./posts/post-list/post-list.component";
 import { AppRoutingModule } from "./app-routing.module";
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
     HeaderComponent,
-    PostListComponent
+    PostListComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
