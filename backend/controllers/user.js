7531a2ea354b72d8c2ae00d5577a468459d53a36
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-05-05 21:39:07 -0300

    Adding controllers

---
+++ backend/controllers/user.js
@@ -0,0 +1,26 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+
+exports.createUser = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then(hash => {
+    const user = new User({
+      email: req.body.email,
+      password: hash
+    });
+    user
+      .save()
+      .then(result => {
+        res.status(201).json({
+          message: "User created!",
+          result: result
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: "Invalid authentication credentials!"
+        });
+      });
+  });
+}
