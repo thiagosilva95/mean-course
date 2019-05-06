15513f7cac30b4a532ec3bbb63a55759b0dd804d
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-05-05 22:09:07 -0300

    Adding controllers

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,22 +3,13 @@ const express = require("express");
 const PostController = require("../controllers/posts");
 
 const checkAuth = require("../middleware/check-auth");
+const extractFile = require("../middleware/file");
 
 const router = express.Router();
 
-router.post(
-  "",
-  checkAuth,
-  multer({ storage: storage }).single("image"),
-  PostController.createPost
-);
+router.post("", checkAuth, extractFile, PostController.createPost);
 
-router.put(
-  "/:id",
-  checkAuth,
-  multer({ storage: storage }).single("image"),
-  PostController.updatePost
-);
+router.put("/:id", checkAuth, extractFile, PostController.updatePost);
 
 router.get("", PostController.getPosts);
 
